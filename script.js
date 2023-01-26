e1cc546e08a94b418c0f91c42976163f43c61afe
Randy DeCent <109132522+RandyDeCent@users.noreply.github.com>
2023-01-26 14:54:20 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function startQuiz(){
   // code to start the quiz
 }
+var questions = [
+  {
+    question: "What is the syntax for creating a function in JavaScript?" 
+  },
+  // add questions 
+];
